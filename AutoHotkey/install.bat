a3a0deebbc543208476a7ebbc2c33d8d6b267092
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-01-21 13:13:56 +0100

    Add install script.

---
+++ AutoHotkey/install.bat
@@ -0,0 +1,49 @@
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS 
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+
+:getPrivileges 
+if '%1'=='ELEV' (shift & goto gotPrivileges)  
+ECHO. 
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation 
+ECHO **************************************
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" 
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs" 
+"%temp%\OEgetPrivileges.vbs" 
+exit /B 
+
+:gotPrivileges 
+::::::::::::::::::::::::::::
+:START
+::::::::::::::::::::::::::::
+setlocal & pushd .
+
+
+REM Run shell as admin (example) - put here code as you like
+cd %~dp0
+
+@echo on
+
+set dest="%userprofile%\Documents\AutoHotkey.ahk"
+set src="%CD%\AutoHotkey.ahk"
+echo "Creating backup of AutoHotkey.ahk"
+del %dest%_backup
+move %dest% %dest%_backup
+mklink %dest% %src%
+
+pause
